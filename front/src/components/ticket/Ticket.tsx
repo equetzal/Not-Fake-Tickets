967c71518a441f7ddca0b0dccaeac9aea053e919
Erik Alcantara <ealcantarac1700@alumno.ipn.mx>
2022-12-16 07:15:14 -0600

    Visual Update to tickets

--- front/src/components/ticket/Ticket.tsx
+++ front/src/components/ticket/Ticket.tsx
@@ -3,12 +3,14 @@ import styles from './Ticket.module.css';
 
 const Ticket: FC = () => {
   return( 
-  <div className={styles.ticket}>
+    <div className={styles.ticket_container}>
+    <img src={require('./../../assets/img/placeholder.png')} />
     <h1>Nombre evento</h1>
     <h3>Descripcion</h3>
     <p>Lugar</p>
     <p>Fecha</p>
-    <h2>Costo</h2>
+    <h2>Costo(s)</h2>
+    <p className={styles.costo_ticket}>$xxx - $xxx</p>
     </div>
   );
 };
